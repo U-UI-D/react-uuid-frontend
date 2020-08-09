650f936cb01bb93fdd3ae513de60223c94de3378
1445654576@qq.com <1445654576@qq.com>
2020-08-09 23:19:59 +0800

    添加组件

---
+++ src/components/al-inline-width-box/ALInlineWidthBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class ALInlineWidthBox extends React.Component{
+  //构造器
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  //渲染函数
+  render() {
+    return(
+      <div style={{display: "inline-block"}} width={this.props.width}>
+        {this.props.children}
+      </div>
+    );
+  }
+
+  //组件挂载完成时调用
+  componentDidMount() {
+
+  }
+
+  //组件卸载前调用
+  componentWillUnmount() {
+
+  }
+
+}
+
+export default ALInlineWidthBox;
