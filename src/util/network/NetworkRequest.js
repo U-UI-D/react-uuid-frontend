57214ea04164ce367c5e7d7333b47d3e019739da
1445654576@qq.com <1445654576@qq.com>
2020-08-09 16:01:24 +0800

    加入将data参数字符串化的功能

--- src/util/network/NetworkRequest.js
+++ src/util/network/NetworkRequest.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "querystring"
 
 //https://jsonplaceholder.typicode.com/posts/1
 let host = "jsonplaceholder.typicode.com";
@@ -7,6 +8,10 @@ let prefix = "";
 let baseURL = "http://" + host + ':' + port + "/" + prefix;
 
 export function request(config) {
+  if (config.type !== "get" || config.type !== "GET"){
+    config.data = qs.stringify(config.data);
+  }
+
   const axiosInstance = axios.create({
     baseURL: baseURL,
     timeout: 30000,
