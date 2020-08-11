f8a7f90ef7aeba04acb50cab4399e1153bf2e43f
1445654576@qq.com <1445654576@qq.com>
2020-08-11 23:48:15 +0800

    添加作品详情页页面

---
+++ src/pages/work/page/WorkDetail.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class WorkDetail extends React.Component{
+  //构造器
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  //渲染函数
+  render() {
+    return(
+      <div>
+        作品详情页：id={this.props.match.params.id}
+      </div>
+    );
+  }
+
+  //组件挂载完成时调用
+  componentDidMount() {
+
+  }
+
+  //组件卸载前调用
+  componentWillUnmount() {
+
+  }
+
+}
+
+export default WorkDetail;
