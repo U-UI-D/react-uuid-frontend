dafe3bb17528d1bf7bb189c0a4e7e63986a67662
AlanLee <1445654576@qq.com>
2020-08-13 23:48:19 +0800

    添加组件

---
+++ src/pages/user/component/Favorite.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Favorite extends React.Component{
+
+  //构造器
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // 渲染函数
+  render() {
+    return (
+        <div>
+          收藏
+        </div>
+    );
+  }
+
+  // 生命周期函数
+  //组件已挂载
+  componentDidMount() {
+
+  }
+
+  //组件将要卸载时
+  componentWillUnmount() {
+
+  }
+
+
+}
+
+export default Favorite;
