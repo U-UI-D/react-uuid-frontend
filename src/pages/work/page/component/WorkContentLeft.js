3fc5cd5977ef24712ec401cbd629f02f52f2c720
1445654576@qq.com <1445654576@qq.com>
2020-08-19 23:40:42 +0800

    添加左边栏组件

---
+++ src/pages/work/page/component/WorkContentLeft.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {Divider} from "antd";
+
+class WorkContentLeft extends React.Component{
+  //构造器
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  //渲染函数
+  render() {
+    return(
+      <div style={{width: 940, backgroundColor: "#fff", padding: 20}}>
+        <h1>科抖-知识类短视频社交APP</h1>
+        <div>
+          <span style={{marginRight: 20}}>原创作品</span>
+          <span style={{marginRight: 20}}>分类：APP</span>
+          <span style={{marginRight: 20}}>版权：APP</span>
+          <span style={{marginRight: 20}}>举报</span>
+        </div>
+
+        <div className="al-m-tb-20px"><Divider /></div>
+
+        <div>
+          <img src="https://img.ui.cn/data/file/0/6/2/3387260.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/1/6/2/3387261.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/2/6/2/3387262.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/3/6/2/3387263.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/4/6/2/3387264.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/5/6/2/3387265.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/6/6/2/3387266.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/7/6/2/3387267.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/8/6/2/3387268.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/9/6/2/3387269.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/0/7/2/3387270.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/1/7/2/3387271.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/2/7/2/3387272.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/3/7/2/3387273.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/4/7/2/3387274.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/5/7/2/3387275.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/6/7/2/3387276.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/7/7/2/3387277.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/8/7/2/3387278.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/9/7/2/3387279.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/0/8/2/3387280.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+          <img src="https://img.ui.cn/data/file/1/8/2/3387281.png?imageMogr2/auto-orient/format/jpg/strip/thumbnail/!1800%3E/quality/90/" style={{width: 100+'%'}} alt=""/>
+        </div>
+      </div>
+    );
+  }
+
+  //组件挂载完成时调用
+  componentDidMount() {
+
+  }
+
+  //组件卸载前调用
+  componentWillUnmount() {
+
+  }
+
+}
+
+export default WorkContentLeft;
