c43afbbf6206a8a3ad752269c130e427c63f1eec
1445654576@qq.com <1445654576@qq.com>
2020-08-19 23:41:03 +0800

    添加右边栏组件

---
+++ src/pages/work/page/component/WorkContentRight.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class WorkContentRight extends React.Component{
+  //构造器
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  //渲染函数
+  render() {
+    return(
+      <div style={{width: 220, backgroundColor: "#fff", padding: 20}}>
+        右边
+      </div>
+    );
+  }
+
+  //组件挂载完成时调用
+  componentDidMount() {
+
+  }
+
+  //组件卸载前调用
+  componentWillUnmount() {
+
+  }
+
+}
+
+export default WorkContentRight;
