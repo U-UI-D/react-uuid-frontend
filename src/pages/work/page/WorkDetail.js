b151c97f7df50047782496cf6063b3648b7e6af0
1445654576@qq.com <1445654576@qq.com>
2020-08-19 23:40:16 +0800

    更新详情页样式

--- src/pages/work/page/WorkDetail.js
+++ src/pages/work/page/WorkDetail.js
@@ -1,4 +1,11 @@
 import React from "react";
+import ALHeader from "../../../components/al-header/ALHeader";
+import "./style.css";
+import WorkContentLeft from "./component/WorkContentLeft";
+import WorkContentRight from "./component/WorkContentRight";
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
 class WorkDetail extends React.Component{
   //构造器
@@ -11,8 +18,20 @@ class WorkDetail extends React.Component{
   //渲染函数
   render() {
     return(
-      <div>
-        作品详情页：id={this.props.match.params.id}
+      <div className="page" style={{width: windowWidth, height: "auto"}}>
+        <div style={{backgroundColor: "#fff"}}>
+          <ALHeader />
+        </div>
+        {/*作品详情页：id={this.props.match.params.id}*/}
+
+        <div style={{height: 30}}></div>
+
+        <div className="content-width">
+          <div className="al-flex-justify-space-between">
+            <WorkContentLeft />
+            <WorkContentRight />
+          </div>
+        </div>
       </div>
     );
   }
