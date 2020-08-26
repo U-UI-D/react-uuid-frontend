be622406d53ca792d8fe0ee0ddc39f3ef103cb3e
AlanLee <1445654576@qq.com>
2020-08-26 16:58:10 +0800

    将函数组件改为class组件

--- src/util/router/RouterView.js
+++ src/util/router/RouterView.js
@@ -2,25 +2,36 @@ import {Route, HashRouter as Router} from "react-router-dom";
 import React from "react";
 import routes from "./config/routers";
 
-
-export const RouteWithSubRoutes  = route => (
+export const RouteWithSubRoutes = route => (
     <Route
         path={route.path}
         exact={route.exact}
         render={props => (
-            <route.component {...props} routes={route.routes} />
+            <route.component {...props} routes={route.routes}/>
         )}
     />
 );
 
-function RouterView() {
-  return (
-      <div>
+class RouterView extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
         <Router>
-          {routes.map((route, i) => <RouteWithSubRoutes key={route.path} {...route} />)}
+          {
+            routes.map((route, i) => {
+              return <RouteWithSubRoutes key={route.path} {...route} />
+            })
+          }
         </Router>
-      </div>
-  );
+    );
+  }
 }
 
 export default RouterView;
