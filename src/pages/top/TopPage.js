e46c2f77d993e4236fd095a8d6d1545f6567d08a
AlanLee <1445654576@qq.com>
2020-09-02 18:15:54 +0800

    添加排行榜页面

---
+++ src/pages/top/TopPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class TopPage extends React.Component{
+
+  //构造器
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // 渲染函数
+  render() {
+    return (
+        <div>
+          排行榜页面
+        </div>
+    );
+  }
+
+  // 生命周期函数
+  //组件已挂载
+  componentDidMount() {
+
+  }
+
+  //组件将要卸载时
+  componentWillUnmount() {
+
+  }
+
+
+}
+
+export default TopPage;
