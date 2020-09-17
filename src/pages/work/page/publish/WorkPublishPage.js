d99aa8bda0dedf312300e57fbc7cd7bf93d50004
AlanLee <1445654576@qq.com>
2020-09-17 23:37:39 +0800

    更新样式

--- src/pages/work/page/publish/WorkPublishPage.js
+++ src/pages/work/page/publish/WorkPublishPage.js
@@ -6,9 +6,10 @@ import {request} from "../../../../util/network/NetworkRequest";
 import ALFlexBox from "../../../../components/al-flex-box/ALFlexBox";
 import ALPlaceBox from "../../../../components/al-place-box/ALPlaceBox";
 import ALTitleBox from "../../../../components/al-title-box/ALTitleBox";
+import {Select, Card, Input} from "antd";
 
 
-class WorkPublishPage extends React.Component{
+class WorkPublishPage extends React.Component {
   constructor(props) {
     super();
     this.state = {
@@ -29,42 +30,69 @@ class WorkPublishPage extends React.Component{
     this.props.history.push({pathname: path, state: data})
   }
 
+  handleChange = (value) => {
+    console.log(`selected ${value}`);
+  }
+
   render() {
     const leftLayout = (
         <ALFlexBox width={200}
-                   className="al-bg-color-white" >
+                   className="al-bg-color-white">
           <ALFlexBox column flexNum={1}>
-            <ALTitleBox hNum={2} isBeauty text={"上传作品"} className="al-m-tb-10px" />
-            <ALTitleBox hNum={2} text={"上传素材"} />
+            <ALTitleBox hNum={2} isBeauty text={"上传作品"} className="al-m-tb-10px"/>
+            <ALTitleBox hNum={2} text={"上传素材"}/>
           </ALFlexBox>
         </ALFlexBox>
     )
+
+    const {Option} = Select;
 
     const rightLayout = (
         <ALFlexBox width={960} padding={30} column
                    className="al-bg-color-white">
-          <h2 className="al-show-border">右边</h2>
-          <h2>右边</h2>
+          <Card title="作品信息" bordered={false}
+                style={{
+                  width: 100 + '%',
+                  lineHeight: 4 + 'em'
+                }}>
+            <div>
+              <Input placeholder="请输入作品名称"/>
+            </div>
+            <div>
+              <Select defaultValue="lucy" style={{width: 120}} onChange={this.handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled" disabled>
+                  Disabled
+                </Option>
+                <Option value="Yiminghe">yiminghe</Option>
+              </Select>
+            </div>
+
+            <div>
+              <Input placeholder="作品说明"/>
+            </div>
+          </Card>
         </ALFlexBox>
     )
 
     return (
-      <div>
-        <div className="al-bg-color-white">
-          <ALHeader />
+        <div>
+          <div className="al-bg-color-white">
+            <ALHeader/>
+          </div>
+          <div className="content-width al-p-tb-20px">
+            <ALFlexBox between>
+              {leftLayout}
+              {rightLayout}
+            </ALFlexBox>
+          </div>
         </div>
-        <div className="content-width al-p-tb-20px" >
-          <ALFlexBox between>
-            {leftLayout}
-            {rightLayout}
-          </ALFlexBox>
-        </div>
-      </div>
     );
   }
 
-}
 
+}
 
 
 export default WorkPublishPage;
