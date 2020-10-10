3352e896fc3fab309c70d01c222d21a877d4ed0c
1445654576@qq.com <1445654576@qq.com>
2020-10-10 23:43:48 +0800

    添加监听事件

--- src/components/al-input/ALInput.js
+++ src/components/al-input/ALInput.js
@@ -1,10 +1,18 @@
 import React, {useState} from "react";
 import ALFlexBox from "../al-flex-box/ALFlexBox";
-import {Input} from "antd";
+import PropTypes from "prop-types";
+import {Input, Select} from "antd";
+import ALPlaceBox from "../al-place-box/ALPlaceBox";
+
+
+const handleChangeSelect = (value) => {
+  console.log(`selected ${value}`);
+}
+
 
 function ALInput(props) {
 
-  const [empty, setEmpty] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
   const [value, setValue] = useState("");
 
   const styles = {
@@ -19,42 +27,86 @@ function ALInput(props) {
 
     text: {
       color: 'orangered',
-      width: '100px'
+      width: '150px'
     }
   }
 
-  return (
-    <ALFlexBox centerV between lineHeight={0}>
-      <ALFlexBox centerV flexNum={1}>
-        <div style={styles.dotRed}/>
-        <div className="al-flex-item-grow-1">
-          <Input style={{borderRadius: 5 + 'px', width: '90%'}}
-                 size="large"
-                 placeholder="请输入作品名称"
-                 maxLength={50}
-                 allowClear
-                 onChange={(e) => {
-                   if (e.target.value.length <= 50){
-                     setValue(e.target.value);
-                   }
-                   setEmpty(e.target.value.length <= 0 ? true : false);
-                   console.log(value);
-                 }}
-                 suffix={<span>
+  const input = (
+    <Input style={{borderRadius: 5 + 'px'}}
+           size="large"
+           placeholder="请输入作品名称"
+           maxLength={50}
+           allowClear
+           onChange={(e) => {
+             if (e.target.value.length <= 50) {
+               setValue(e.target.value);
+               props.onChange(e.target.value);
+             }
+             setIsEmpty(e.target.value.length <= 0 ? true : false);
+           }}
+           suffix={<span>
                  {
                    value.length >= 50 ? 0 : (50 - value.length)
                  }
                </span>}
-          />
+    />
+  );
+
+  const textarea = (
+    <Input.TextArea style={{borderRadius: 5 + 'px'}}
+                    autoSize={{minRows: 3}}
+                    size="large"
+                    placeholder="作品说明"
+                    onChange={(e) => {
+                      props.onChange(e.target.value);
+                      // console.log(e.target.value);
+                    }}/>
+  );
+
+  const select = (
+    <div className="al-display-inline-block">
+      <Select
+        placeholder="请选择类型"
+        style={{width: 120}}
+        onChange={handleChangeSelect}>
+        <Select.Option value="work">作品</Select.Option>
+        <Select.Option value="icon">icon</Select.Option>
+        <Select.Option value="image">图片</Select.Option>
+      </Select>
+    </div>
+  );
+
+
+  return (
+    <ALFlexBox centerV between lineHeight={0}>
+      <ALFlexBox centerV flexNum={1}>
+        {
+          props.required ? <div style={styles.dotRed}/> : <ALPlaceBox width={28}/>
+        }
+        <div className="al-flex-item-grow-1">
+          {props.type === "textarea" ? textarea : (props.type === "select" ? select : input)}
         </div>
       </ALFlexBox>
       <div className="al-text-right" style={styles.text}>
         {
-          empty ? <span>必填项</span> : <span></span>
+          isEmpty ? <span>必填项</span> : <span></span>
         }
       </div>
     </ALFlexBox>
   );
+}
+
+ALInput.propTypes = {
+  type: PropTypes.string,
+  required: PropTypes.bool,
+  onChange: PropTypes.func,
+}
+
+// 默认值
+ALInput.defaultProps = {
+  type: "input",
+  required: false,
+  onChange: () => {}
 }
 
 export default ALInput;
