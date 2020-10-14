406aac6bf9439df878445bb4ff8b4df96383f3c1
1445654576@qq.com <1445654576@qq.com>
2020-10-14 19:31:16 +0800

    更新样式

--- src/pages/user/page/profile/component/BaseInfoTabCard.js
+++ src/pages/user/page/profile/component/BaseInfoTabCard.js
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
 import {getUserInfoFromLocalStorage} from "../../../../../util/util";
-import {Upload, Modal, Avatar, Button, Col, Input, Radio, Row, message} from "antd";
+import {Upload, Modal, Avatar, Button, Input, Radio, message} from "antd";
 import ALFlexBox from "../../../../../components/al-flex-box/ALFlexBox";
 import ALLabelBox from "../../../../../components/al-label-box/ALLabelBox";
 import ALImage from "../../../../../components/al-image/ALImage";
 import ImgCrop from 'antd-img-crop';
+import ALPlaceBox from "../../../../../components/al-place-box/ALPlaceBox";
+import {commonRequest} from "../../../../../util/network/RequestHub";
+import {PUT_USER} from "../../../../../util/network/config/ApiConst";
+import {getCookieByName} from "../../../../../util/cookieUtil";
 
 
 function BaseInfoTabCard(props) {
@@ -14,6 +18,12 @@ function BaseInfoTabCard(props) {
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState("");
   const [sendData, sendSendData] = useState({});
+
+  const [nickname, setNickname] = useState(userInfo.nickname ?? "");
+  const [age, setAge] = useState(userInfo.age ?? "");
+  const [phone, setPhone] = useState(userInfo.phone ?? "");
+  const [email, setEmail] = useState(userInfo.email ?? "");
+  const [sign, setSign] = useState(userInfo.personalitySign ?? "");
 
 
   const showModal = () => {
@@ -47,9 +57,20 @@ function BaseInfoTabCard(props) {
 
   const saveUserInfo = () => {
     let sendData = {
-      ...userInfo
+      ...userInfo,
+      avatar,
+      createdTime: null,
+      updatedTime: new Date()
     }
     console.log("sendData", sendData);
+
+    let token = getCookieByName("sso_token");
+    commonRequest({url: PUT_USER, method: "PUT", data: sendData, headers: {
+      "Authorization": token
+      }})
+      .then(res => {
+        console.log("PUT_USER", res);
+      })
   }
 
   const uploadAvatar = {
@@ -61,10 +82,11 @@ function BaseInfoTabCard(props) {
     onChange(info) {
       if (info.file.status !== 'uploading') {
         console.log(info.file, info.fileList);
-        setAvatar(info.file.response);
       }
       if (info.file.status === 'done') {
         message.success(`${info.file.name} 上传成功`);
+        console.log(info.file, info.fileList);
+        setAvatar(info.file.response);
         console.log("avatar", avatar)
       } else if (info.file.status === 'error') {
         message.error(`${info.file.name} 上传失败`);
@@ -112,7 +134,7 @@ function BaseInfoTabCard(props) {
       <h1>基本信息</h1>
 
       {/*头像*/}
-      <ALFlexBox centerVH column>
+      <ALFlexBox centerVH column className="al-m-tb-20px">
         <div onMouseEnter={() => {
           setMouseIn(true);
         }}
@@ -120,7 +142,7 @@ function BaseInfoTabCard(props) {
                setMouseIn(false);
              }}
              className="al-position-rela">
-          <Avatar src={userInfo.avatar} size={120}/>
+          <Avatar src={avatar === "" ? userInfo.avatar : avatar} size={120}/>
           {
             // 鼠标移入头像后的操作
             mouseIn ? (
@@ -136,10 +158,6 @@ function BaseInfoTabCard(props) {
             ) : ""
           }
         </div>
-
-        <div className="al-p-tb-20px">
-          <Button type="link">修改头像</Button>
-        </div>
       </ALFlexBox>
 
       {/*表单信息*/}
@@ -153,17 +171,33 @@ function BaseInfoTabCard(props) {
             <ALLabelBox label={"昵称"}>
               <Input defaultValue={userInfo.nickname}
                      onChange={(e) => {
+                       setNickname(e.target.value);
                        userInfo.nickname = e.target.value;
-                       setUserInfo(userInfo)
-                     }}/>
+                       setUserInfo(userInfo);
+                     }}
+                     allowClear
+                     maxLength={16}
+                     suffix={
+                       <span className="suffix-text">
+                         {(16 - nickname.length)}
+                       </span>
+                     }/>
             </ALLabelBox>
 
             <ALLabelBox label={"手机"}>
               <Input defaultValue={userInfo.phone}
                      onChange={(e) => {
+                       setPhone(e.target.value);
                        userInfo.phone = e.target.value;
-                       setUserInfo(userInfo)
-                     }}/>
+                       setUserInfo(userInfo);
+                     }}
+                     allowClear
+                     maxLength={11}
+                     suffix={
+                       <span className="suffix-text">
+                         {(11 - phone.length)}
+                       </span>
+                     }/>
             </ALLabelBox>
           </ALFlexBox>
 
@@ -177,7 +211,7 @@ function BaseInfoTabCard(props) {
                              }}>
                   <Radio value={"1"}>男</Radio>
                   <Radio value={"0"}>女</Radio>
-                  <Radio value={null}>保密</Radio>
+                  <Radio value={"2"}>保密</Radio>
                 </Radio.Group>
               </div>
             </ALLabelBox>
@@ -185,24 +219,54 @@ function BaseInfoTabCard(props) {
             <ALLabelBox label={"年龄"}>
               <Input defaultValue={userInfo.age}
                      onChange={(e) => {
+                       if (e.target.value > 120) {
+                         message.error("年龄超出正常范围");
+                         return;
+                       }
+                       setAge(e.target.value);
                        userInfo.age = e.target.value;
-                       setUserInfo(userInfo)
-                     }}/>
+                       setUserInfo(userInfo);
+                     }}
+                     allowClear
+                     maxLength={3}
+                     suffix={
+                       <span className="suffix-text">
+                         {(3 - age.toString().length)}
+                       </span>
+                     }/>
             </ALLabelBox>
 
             <ALLabelBox label={"邮箱"}>
               <Input defaultValue={userInfo.email}
                      onChange={(e) => {
+                       setEmail(e.target.value);
                        userInfo.email = e.target.value;
-                       setUserInfo(userInfo)
-                     }}/>
+                       setUserInfo(userInfo);
+                     }}
+                     allowClear
+                     maxLength={32}
+                     suffix={
+                       <span className="suffix-text">
+                         {32 - email.length}
+                       </span>
+                     }/>
             </ALLabelBox>
           </ALFlexBox>
         </ALFlexBox>
 
-        <div className="al-m-bottom-20px">
+        <div className="al-m-top-10px al-m-bottom-20px">
           <ALLabelBox label={"个性签名"}>
-            <Input.TextArea rows={5}/>
+            <Input.TextArea rows={3}
+                            maxLength={100}
+                            allowClear
+                            onChange={(e) => {
+                              setSign(e.target.value);
+                              userInfo.personalitySign = e.target.value;
+                              setUserInfo(userInfo);
+                            }}/>
+            <div className="suffix-text al-text-right">
+              {sign.length} / 100
+            </div>
           </ALLabelBox>
         </div>
 
